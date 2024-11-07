d3f1f05e07981557beaea083815079100de3b3a0
rafael864 <95891681+rafael864@users.noreply.github.com>
2024-11-07 06:48:37 +0200

    Update info.py

--- View/Python/ShowOnePatient/info.py
+++ View/Python/ShowOnePatient/info.py
@@ -3,11 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email
 from openpyxl import load_workbook
 class patientForm(FlaskForm):
-    name = StringField('Name',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    id = PasswordField('Id', validators=[DataRequired()])
-    age = PasswordField('Age ',
-                                     validators=[DataRequired()])
+    name = StringField('Name',validators=[DataRequired(), Length(min=2, max=20)])
+    id = IntegerField('Id', validators=[DataRequired()])
+    diseas = StringField('Diseas',validators=[DataRequired()])
+    age = IntegerField('Age ',validators=[DataRequired()])
     submit = SubmitField('save')
