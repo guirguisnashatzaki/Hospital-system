51f0a651840221160c8ce63cc0c43daa1b4ffe41
rafael864 <95891681+rafael864@users.noreply.github.com>
2024-11-07 05:38:08 +0200

    Update File_Handler.py

--- Files/File_Handler.py
+++ Files/File_Handler.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import Staff
+from Patient import patients
 class file_handler():
     
     staff_path = ""
@@ -9,15 +11,25 @@ class file_handler():
         self.staff_path = staff_path
     
     def getStaff(self):
-        patient_file = pd.read_excel('patient.xlsx')
-        del patient_file[patient_file.columns[0]]
-        
+        staff_file = pd.read_excel(staff_path)
+        del staff_file[staff_file.columns[0]]
+        staff_list = []
+        for i in staff_file.keys():
+            s = staff(i,i[0],i[1],i[2])
+            staff_list.append(s)
+            return staff_list
     
     def saveStaff(self,staff):
         pass
     
     def getPatients(self):
-        pass
+        patients_file = pd.read_excel(patients_path)
+        del patients_file[patients_file.columns[0]]
+        patients_list = []
+        for i in patient_file.keys():
+            s = patients(i,i[0],i[1],i[2])
+            patients_list.append(s)
+            return patients_list
     
     def savePatient(self,patient):
         pass
